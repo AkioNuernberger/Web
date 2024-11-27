43d832d232a54dea311434088ad31eb37b1a5bab
Akio <akio@Akios-MacBook-Pro.local>
2024-11-27 14:08:52 +0100

    track new page

---
+++ akio-web/app/pages/user-manual-akio/page.tsx
@@ -0,0 +1,9 @@
+export default function ManualAkio() {
+  return (
+    <div className="content-container">
+      <h1 className="text-3xl font-bold mb-4">Manual - Akio</h1>
+      <p>This is the Manual - Akio page content.</p>
+    </div>
+  )
+}
+

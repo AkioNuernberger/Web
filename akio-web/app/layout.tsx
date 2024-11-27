8349deeef238eb05ba1f60b06e099dd3b9ece9f5
Akio <akio@Akios-MacBook-Pro.local>
2024-11-27 11:53:10 +0100

    remove gtag

--- akio-web/app/layout.tsx
+++ akio-web/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Navbar from '@/components/Navbar'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,9 +18,21 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} flex flex-col sm:flex-row`}>
+      <body className={inter.className}>
         <Navbar />
         <main className="flex-grow sm:ml-48 p-4 sm:p-6 lg:p-8 mt-16 sm:mt-0">{children}</main>
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+          `}
+        </Script>
       </body>
     </html>
   )

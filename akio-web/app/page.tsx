b5969df97a79a6a6b8cf334c014fc91abbafbb23
Akio <akio@Akios-MacBook-Pro.local>
2025-01-27 12:32:56 +0900

    text change with additional clarification

--- akio-web/app/page.tsx
+++ akio-web/app/page.tsx
@@ -13,7 +13,8 @@ export default function Home() {
           </h1>
           <p className="mt-3 text-base text-gray-700 sm:mt-5 sm:text-lg md:mt-5 md:text-xl">
             <span className="block mb-4">I&apos;m Akio. Operator and entrepreneur, currently based in SF.</span>
-            <span className="block">I believe that transparency makes collaboration easier and created this page to share more about my principles and thoughts.</span>
+            
+            <span className="block">This was originally the website for the project tangential Inc., a software to automate tasks for Technical Program Managers. I now changed it to a site sharing more about my principles and thoughs as I believe that transparency makes collaboration easier.</span>
           </p>
           <div className="mt-5 sm:mt-8">
             <Link href="/pages/user-manual-akio">
